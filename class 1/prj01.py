99b18a9581f6c8e572ad53a5c00f66560ce62754
Andre-Wang111 <andrewang0729@gmail.com>
2025-04-10 20:34:43 +0800

    新增遊戲初始化及主循環邏輯

--- class 1/prj01.py
+++ class 1/prj01.py
@@ -1,1 +1,16 @@
+######################匯入模組######################
+import pygame
+import sys
 
+######################初始化######################
+pygame.init()
+width = 640
+height = 320
+######################建立視窗及物件######################
+screen = pygame.display.set_mode((width, height))
+pygame.display.set_caption("My Game")
+######################循環偵測######################
+while True:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            sys.exit()
